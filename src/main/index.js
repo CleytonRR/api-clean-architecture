b99a9cfbe93ea2df4107872e2b448c1beb71a35a
cleytonRR <cleytonrodrigues69@live.com>
2020-09-06 18:33:45 -0300

    Refactor

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,3 @@
-const express = require('express')
-const app = express()
+const app = require('./config/app')
 
-app.disable('X-powered-by')
-app.get('/mango', (req, res) => {
-  res.send('Mango')
-})
 app.listen(5858, () => console.log('Server Running'))
