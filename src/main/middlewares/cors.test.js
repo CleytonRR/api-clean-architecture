9f76e9dca57c248bc32f0e0a71871243054c5545
cleytonRR <cleytonrodrigues69@live.com>
2020-09-06 18:54:56 -0300

    Rename route

--- src/main/middlewares/cors.test.js
+++ src/main/middlewares/cors.test.js
@@ -6,7 +6,7 @@ describe('CORS Middleware', () => {
     app.get('/test_cors', (req, res) => {
       res.send('')
     })
-    const res = await request(app).get('/test_x_powered_by')
+    const res = await request(app).get('/test_cors')
     expect(res.headers['access-control-allow-origin']).toBe('*')
     expect(res.headers['access-control-allow-methods']).toBe('*')
     expect(res.headers['access-control-allow-headers']).toBe('*')
