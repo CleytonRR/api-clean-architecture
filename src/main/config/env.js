407c204709947755758c48177b28f9b0326e7762
cleytonRR <cleytonrodrigues69@live.com>
2020-10-11 23:53:12 -0300

    Added token secret env file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xx/clean-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xx/clean-node-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
